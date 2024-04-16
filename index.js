d83fe43a97824d7e6af22dce24c5a6d3a7f6abf8
Arick Castillo <arick.castillo91@gmail.com>
2024-04-15 18:09:16 -0600

    se modifico index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hellow Motherfucker");
+const isOdd = require("is-odd");
+
+console.log(isOdd("1")); //=> true
+console.log(isOdd("3")); //=> true
+
+console.log(isOdd(0)); //=> false
+console.log(isOdd(2)); //=> false
